e439e73e4b7005c8d277e200e96a13d65fd0d822
VeliG <vgebrev@gmail.com>
2020-07-29 23:01:18 +0200

    feat: Add colour combinations

--- week-04/day-022-crates/src/arts.rs
+++ week-04/day-022-crates/src/arts.rs
@@ -7,6 +7,7 @@ pub use self::kinds::SecondaryColor;
 pub use self::utils::mix;
 
 pub mod kinds {
+    #[derive(Debug,PartialEq)]
     /// The primary colors according to the RYB color model.
     pub enum PrimaryColor {
         Red,
@@ -14,6 +15,7 @@ pub mod kinds {
         Blue,
     }
 
+    #[derive(Copy, Clone)]
     /// The secondary colors according to the RYB color model.
     pub enum SecondaryColor {
         Orange,
@@ -28,6 +30,15 @@ pub mod utils {
     /// Combines two primary colors in equal amounts to create
     /// a secondary color.
     pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
-        SecondaryColor::Purple
+        let palette = vec![
+            (PrimaryColor::Red, PrimaryColor::Yellow, SecondaryColor::Orange),
+            (PrimaryColor::Red, PrimaryColor::Blue, SecondaryColor::Purple),
+            (PrimaryColor::Yellow, PrimaryColor::Blue, SecondaryColor::Green)
+        ];
+        if let Some(item) = palette.iter().find(|p| (p.0 == c1 && p.1 == c2) || (p.0 == c2 && p.1 == c1)) {
+            item.2
+        } else {
+            panic!("No combination found for {:?} {:?}", c1, c2);
+        }
     }
 }
