2031016185e0f4015d8b95349096778ba5d2e146
VeliG <vgebrev@gmail.com>
2020-08-17 22:00:30 +0200

    feat: Day 24. Chapter 14.3. Workspaces

---
+++ week-04/day-024-workspaces/add/add-one/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_one_success() {
+        assert_eq!(3, add_one(2));
+    }
+}
